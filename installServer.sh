9263e1720da0e04446e0449aa71531c0cb2d44cf
mmalaaksonen <mmalaaksonen@users.noreply.github.com>
2015-03-16 21:02:04 +0200

    Update installServer.sh

--- installServer.sh
+++ installServer.sh
@@ -1,10 +1,9 @@
+#!/bin/bash
 sudo sh
 apt-get install git openjdk-7-jre tar curl
 curl -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
 git config --global --unset core.autocrlf
 java -jar BuildTools.jar
-
-shopt -s extglob
 
 rm server.jar
 rm start.sh
@@ -15,9 +14,11 @@ chmod +x server.jar
 curl -o start.sh https://raw.githubusercontent.com/mmalaaksonen/misc/master/mcserverstart.sh
 chmod +x start.sh
 
-find . -type f -not -name 'server.jar' -not -name 'start.sh' -not -name 'installServer.sh' | xargs rm -r
+shopt -s extglob
+rm -r !(installServer.sh|server.jar|start.sh)
 
-exit
 clear
 
-./start.sh
+exit
+
+xterm -e ./start.sh
