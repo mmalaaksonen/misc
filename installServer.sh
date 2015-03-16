2cf3ee66ecf07bfa8adf07e00d857a78bbebbb5e
mmalaaksonen <mmalaaksonen@users.noreply.github.com>
2015-03-16 22:07:21 +0200

    Update installServer.sh

--- installServer.sh
+++ installServer.sh
@@ -12,11 +12,6 @@ echo 'Do you want to proceed [y/n]? ' | tr -d '\012' ; read
 if [ "_$REPLY" = "_y" ]; then
     shopt -s extglob
     rm -r !(installServer.sh)
-else
-    echo '(cancelled)'
-    exit
-fi
-
 curl -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
 git config --global --unset core.autocrlf
 java -jar BuildTools.jar
@@ -31,5 +26,9 @@ shopt -s extglob
 rm -r !(installServer.sh|server.jar|start.sh)
 
 read -p "Open start.sh through a terminal to start server."
+else
+    echo '(cancelled)'
+    rm installServer.sh
+fi
 
-rm installServer.sh
+
