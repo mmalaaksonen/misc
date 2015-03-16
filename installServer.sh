d42e0beedf7990846d7552a0aa1d67ddaf5205c6
mmalaaksonen <mmalaaksonen@users.noreply.github.com>
2015-03-16 21:27:40 +0200

    Update installServer.sh

--- installServer.sh
+++ installServer.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 sudo sh
 apt-get install git openjdk-7-jre tar curl
-curl -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
-git config --global --unset core.autocrlf
-java -jar BuildTools.jar
 
 curl -o del3314.sh https://raw.githubusercontent.com/mmalaaksonen/misc/master/mcdirdelete.sh
 xterm -e ./del3314.sh
+
+curl -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+git config --global --unset core.autocrlf
+java -jar BuildTools.jar
 
 mv spigot*.jar server.jar
 chmod +x server.jar
@@ -14,6 +15,9 @@ chmod +x server.jar
 curl -o start.sh https://raw.githubusercontent.com/mmalaaksonen/misc/master/mcserverstart.sh
 chmod +x start.sh
 
-clear
+shopt -s extglob
+rm -r !(installServer.sh|server.jar|start.sh)
 
 xterm -e ./start.sh
+
+rm installServer.sh
