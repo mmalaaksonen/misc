7098237c34a221a0e637e6e67dd184ecf06f1edb
mmalaaksonen <mmalaaksonen@users.noreply.github.com>
2015-03-16 21:21:20 +0200

    Update installServer.sh

--- installServer.sh
+++ installServer.sh
@@ -5,8 +5,8 @@ curl -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSucce
 git config --global --unset core.autocrlf
 java -jar BuildTools.jar
 
-rm server.jar
-rm start.sh
+curl -o del3314.sh https://raw.githubusercontent.com/mmalaaksonen/misc/master/mcdirdelete.sh
+xterm -e ./del3314.sh
 
 mv spigot*.jar server.jar
 chmod +x server.jar
@@ -14,11 +14,6 @@ chmod +x server.jar
 curl -o start.sh https://raw.githubusercontent.com/mmalaaksonen/misc/master/mcserverstart.sh
 chmod +x start.sh
 
-shopt -s extglob
-rm -r !(installServer.sh|server.jar|start.sh)
-
 clear
-
-exit
 
 xterm -e ./start.sh
