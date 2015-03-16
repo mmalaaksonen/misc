6dcce8410ef9bff47db3ea7a04bc93791e3f8ff1
mmalaaksonen <mmalaaksonen@users.noreply.github.com>
2015-03-16 21:36:57 +0200

    Update installServerInit.sh

--- installServerInit.sh
+++ installServerInit.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 curl -o installServer.sh https://raw.githubusercontent.com/mmalaaksonen/misc/master/installServer.sh
+chmod +x installServer.sh
 xterm -e ./installServer.sh
 rm installServerInit.sh
